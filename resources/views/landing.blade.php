466b533443c0a23b702bf74e2be57520482aae7d
rihards123-web <rihardsnol@gmail.com>
2026-04-04 21:41:03 +0300

    adds landing pages first section

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,6 +9,41 @@
 </head>
 <body>
     @include('components.nav-bar')
-    
+    <section class="landing">
+        <div class="info-container">
+            <h1>
+                Quality, <br>
+                Safety, <br>
+                Transparency.
+            </h1>
+
+            <div class="info-container-inventory">
+                <a href="">
+                View full inventory
+                </a>
+                <img src="{{ Vite::asset('resources/images/keyboard_arrow_right.svg') }}" alt="arrow">  
+            </div>
+        </div>
+
+         <div class="img-container">
+            <img src="{{ Vite::asset('resources/images/porsche-model.png') }}" alt="Car">
+        </div>
+    </section>
+
+    <section class="arrow-down">
+        <img src="{{ Vite::asset('resources/images/keyboard_arrow_down.svg') }}" alt="Car">
+    </section>
+
+    <section class="landing-display-models">
+        <div class="display-heading">
+            <h2>
+                Available Models
+            </h2>
+        </div>
+
+        <div class="display-models">
+            
+        </div>
+    </section>
 </body>
 </html>
