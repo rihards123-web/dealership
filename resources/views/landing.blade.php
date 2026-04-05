76c5487d70bf6b89015e7c83f6a312f8a2835199
rihards123-web <rihardsnol@gmail.com>
2026-04-05 21:31:53 +0300

    works on the landing page, makes elements be responsive to smaller, and larger screen resolutions.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -35,14 +35,50 @@
     </section>
 
     <section class="landing-display-models">
-        <div class="display-heading">
-            <h2>
-                Available Models
-            </h2>
+        <div class="models-content">
+            <div class="display-heading">
+                <h2>Models</h2>
+            </div>
+
+            <div class="display-models">
+                <div class="display-models-combustion">
+                    <img src="{{ Vite::asset('resources/images/Combustion.png') }}" alt="Combustion" id="car_image">
+                </div>
+                <div class="display-models-hybrid">
+                    <img src="{{ Vite::asset('resources/images/Hybrid.png') }}" alt="Hybrid" id="car_image">
+                </div>
+                <div class="display-models-electric">
+                    <img src="{{ Vite::asset('resources/images/Electric.png') }}" alt="Electric" id="car_image">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="email-section">
+        <div class="email-section-info-text">
+            <h2>Are you looking for a specific model?</h2>
+            <p>Leave your contact information so our team can get in touch with you!</p>
         </div>
 
-        <div class="display-models">
-            
+        <div class="email-container">
+                <form action="">
+                    <div class="form-row">
+                        <input type="text" name="" id="form-input" placeholder="Model" required>
+                        <input type="text" name="" id="form-input" placeholder="Name" required>
+                        <input type="email" name="" id="form-input" placeholder="E-mail" required>
+                        <input type="tel" name="" id="form-input" placeholder="Number" required>
+                        <input type="submit" name="" id="form-submit">
+                    </div>
+                   
+                    <div class="checkbox-row">
+                           <label for="">
+                                <input type="checkbox"> 
+                                  By LGPD terms, I agree to provide the data above so that Touring Cars can contact me to
+                                  present services. Your name, email, and phone number will be used
+                                  in accordance with our Privacy Policy.
+                            </label>
+                    </div>
+                </form>
         </div>
     </section>
 </body>
